92c77987586ea44213964d01aa591ee3ed984a8e
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-06-15 15:38:51 +0200

    feat: implement megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,28 @@
 /*   By: ladloff <ladloff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:40:03 by ladloff           #+#    #+#             */
-/*   Updated: 2024/04/18 13:40:03 by ladloff          ###   ########.fr       */
+/*   Updated: 2024/06/15 15:36:57 by ladloff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			{
+				std::cout << (char)toupper(argv[i][j]);
+			}
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
